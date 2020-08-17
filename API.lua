d1af0aafde2dd09ec7b4af746076e18ac22f3ccc
ElKoax <47944778+ElKoax@users.noreply.github.com>
2020-08-17 02:39:41 +0100

    Update API.lua

--- API.lua
+++ API.lua
@@ -12,4 +12,9 @@ Returns a Table with Roblox's Environment.
 table getreg(void)
 Returns a Table Populated with the Roblox Registry Index.
 
+function newcclosure(function name)
+Very useful for metatable hooks its also yield-able.
+    
+table/userdata getrawmetatable(table/userdata)
+returns a metatable based on the <Table> or <Userdata> given
 
