f6daa4db8976a6b51f7472ed2aa3f9fff00236f9
ElKoax <47944778+ElKoax@users.noreply.github.com>
2020-08-17 02:32:34 +0100

    Update API.lua

--- API.lua
+++ API.lua
@@ -1,1 +1,15 @@
+-- This is ScreamSploits Custom Function List!
+
+table getgc(bool true| false)
+Returns a Table with all Garbage Collected Objects.
+
+table getgenv(void)
+Returns a Table populated with ScreamSploits Environment.
+
+table getrenv(void)
+Returns a Table with Roblox's Environment.
+
+table getreg(void)
+Returns a Table Populated with the Roblox Registry Index.
+
 
